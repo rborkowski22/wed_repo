47fab829f55709b01aa71ac7a866d82318b562b2
rborkowski22 <35058643+rborkowski22@users.noreply.github.com>
2022-08-02 23:27:29 +0200

    add help

---
+++ src/main/java/eu/wed/WedApplication.java
@@ -0,0 +1,13 @@
+package eu.wed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eu/wed/WedApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.wed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
