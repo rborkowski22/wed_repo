aa23047712ca100af064ce0b1100cfa195708f9c
rafal.borkowski <rafal.borkowski@sollers.eu>
2022-08-21 12:45:46 +0200

    adds mysql db connection

--- src/main/java/eu/wed/controllers/ImageGalleryController.java
+++ src/main/java/eu/wed/controllers/ImageGalleryController.java
@@ -1,13 +1,28 @@
 package eu.wed.controllers;
 
+import eu.wed.model.Image;
+import eu.wed.repositories.ImageRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ImageGalleryController {
 
+    @Autowired
+    private ImageRepository imageRepository;
+
     @GetMapping({"", "/", "/index", "/home"})
     public String getHomePage() {
         return "index";
     }
+
+    @GetMapping({"/test"})
+    public String test() {
+        Image image = new Image("testName", null, 1L);
+        this.imageRepository.save(image);
+        return "test";
+    }
+
+
 }
