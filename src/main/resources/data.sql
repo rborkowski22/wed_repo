8f9cf911951bdc8b5716af01bdd4ebc494564a13
rborkowski22 <35058643+rborkowski22@users.noreply.github.com>
2022-08-30 08:50:01 +0200

    Wd 2 upload multiple files (#3)
    * WD-2 add admin page with multiple photos upload
    
    * WD-2 adjust database and add trigger to copy id

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,10 @@
-INSERT INTO IMAGE (NAME, ORDER_NUMBER) VALUES ('first name', 1);
+USE wedgallery;
+
+create TRIGGER IF NOT EXISTS COPY_ID
+    BEFORE INSERT
+    ON IMAGE
+    FOR EACH ROW
+    SET NEW.ORDER_NUMBER = (select AUTO_INCREMENT
+                            from information_schema.TABLES
+                            where table_schema = database()
+                              and TABLE_NAME = 'IMAGE');
--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,40 +1,30 @@
 
 $(document).ready(function () {
-    $('#loader').hide();
     $("#submit").on("click", function () {
-        // $("#submit").prop("disabled", true);
         var name = $("#name").val();
-        var file = $("#image").val();
-        var description = $("#description").val();
+        // var files = document.getElementById('image').files;
         var form = $("#form").serialize();
-        var data = new FormData($("#form")[0]);
-        data.append('name', name);
-        // data.append('description', description);
-        //alert(data);
-        if (name === "" || file === ""/* || price === "" || description === ""*/) {
+        var files = new FormData($("#form")[0]);
+        // $.each(files, function (index) {
+        //     data.append(files[index].name, files[index]);
+        // });
+              for (var [a, b] of files.entries()) {
+                   console.log(a, b);
+               }
+        if (files === null) {
             $("#submit").prop("disabled", false);
-            // $('#loader').hide();
             $("#name").css("border-color", "red");
             $("#image").css("border-color", "red");
-            // $("#price").css("border-color", "red");
-            // $("#description").css("border-color", "red");
-            $("#error_name").html("Please fill the required field.");
             $("#error_file").html("Please fill the required field.");
-            // $("#error_price").html("Please fill the required field.");
             // $("#error_description").html("Please fill the required field.");
         } else {
-            $("#name").css("border-color", "");
             $("#image").css("border-color", "");
-            // $("#price").css("border-color", "");
-            // $("#description").css("border-color", "");
             $('#error_name').css('opacity', 0);
             $('#error_file').css('opacity', 0);
-            // $('#error_price').css('opacity', 0);
-            // $('#error_description').css('opacity', 0);
             $.ajax({
                 type:'POST',
                 enctype: 'multipart/for-data',
-                data: data,
+                data: files,
                 url: "/upload",
                 processData: false,
                 contentType: false,
@@ -52,7 +42,6 @@ $(document).ready(function () {
                   }
                 },
                 error: function (e) {
-                    // $('#loader').hide();
                     $('#error').css('display','block');
                     $("#error").html("Oops! something went wrong.");
                     $('#error').delay(5000).fadeOut('slow');
